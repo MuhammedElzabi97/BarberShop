da9d5f9ab95fcca398e0279e6b05519d3a69ac03
MuhammedElzabi97 <muhammed.elzabi@ogr.sakarya.edu.tr>
2024-12-23 23:48:19 +0300

    Update Randevu.cs

--- Models/Randevu.cs
+++ Models/Randevu.cs
@@ -1,16 +1,16 @@
-﻿namespace Barber_Shop.Models
+public class Randevu
 {
-    public class Randevu
-    {
-        public int RandevuId { get; set; }
-        public int KullaniciId { get; set; }
-        public Kullanici? Kullanici { get; set; }
-        public int CalisanId { get; set; }
-        public Calisan? Calisan { get; set; }
-        public int HizmetId { get; set; }
-        public Hizmet? Hizmet { get; set; }
-        public DateTime Tarih { get; set; } 
-        public TimeSpan Saat { get; set; } 
-        public string Durum { get; set; } = "Beklemede";
-    }
+    public int RandevuId { get; set; } // PK
+    public DateTime Tarih { get; set; }
+    public string Saat { get; set; }
+
+    public int HizmetId { get; set; } // FK
+    public Hizmet Hizmet { get; set; }
+
+    public int CalisanId { get; set; } // FK
+    public Calisan Calisan { get; set; }
+
+    public int KullaniciId { get; set; } // FK
+    public Kullanici Kullanici { get; set; }
 }
+
