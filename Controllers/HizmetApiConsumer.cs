a5b1f4e8b0212051aa50447cee516ae3271c6257
MuhammedElzabi97 <muhammed.elzabi@ogr.sakarya.edu.tr>
2024-12-28 23:33:18 +0300

    Add files via upload

---
+++ Controllers/HizmetApiConsumer.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebProjesi.Models;
+
+namespace HizmetApiConsumer
+{
+    public class HizmetApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HizmetApiClient(string baseUrl)
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseUrl)
+            };
+        }
+
+        public async Task<IEnumerable<Hizmet>> GetAllHizmetlerAsync()
+        {
+            var response = await _httpClient.GetAsync("api/HizmetApi");
+            response.EnsureSuccessStatusCode();
+
+            var hizmetler = await response.Content.ReadFromJsonAsync<IEnumerable<Hizmet>>();
+            return hizmetler ?? new List<Hizmet>();
+        }
+
+        public async Task<Hizmet> GetHizmetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"api/HizmetApi/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Hizmet not found with ID {id}");
+            }
+
+            var hizmet = await response.Content.ReadFromJsonAsync<Hizmet>();
+            return hizmet!;
+        }
+
+        public async Task<Hizmet> CreateHizmetAsync(Hizmet yeniHizmet)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/HizmetApi", yeniHizmet);
+            response.EnsureSuccessStatusCode();
+
+            var createdHizmet = await response.Content.ReadFromJsonAsync<Hizmet>();
+            return createdHizmet!;
+        }
+
+        public async Task UpdateHizmetAsync(int id, Hizmet guncelHizmet)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/HizmetApi/{id}", guncelHizmet);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteHizmetAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/HizmetApi/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ Controllers/HizmetApiController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProjesi.Data.Services;
+using WebProjesi.Models;
+
+namespace WebProjesi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HizmetApiController : ControllerBase
+    {
+        private readonly IHizmetServices _hizmetService;
+
+        public HizmetApiController(IHizmetServices hizmetService)
+        {
+            _hizmetService = hizmetService;
+        }
+
+        // GET: api/HizmetApi
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var hizmetler = _hizmetService.GetAll();
+            return Ok(hizmetler);
+        }
+
+        // GET: api/HizmetApi/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var hizmet = _hizmetService.GetById(id);
+            if (hizmet == null)
+            {
+                return NotFound();
+            }
+            return Ok(hizmet);
+        }
+
+        // POST: api/HizmetApi
+        [HttpPost]
+        public IActionResult Create([FromBody] Hizmet hizmet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _hizmetService.YeniHizmetEkle(hizmet);
+            return CreatedAtAction(nameof(GetById), new { id = hizmet.HizmetID }, hizmet);
+        }
+
+        // PUT: api/HizmetApi/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Hizmet guncelHizmet)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var mevcutHizmet = _hizmetService.GetById(id);
+            if (mevcutHizmet == null)
+            {
+                return NotFound();
+            }
+
+            _hizmetService.HizmetGuncelle(id, guncelHizmet);
+            return NoContent();
+        }
+
+        // DELETE: api/HizmetApi/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var mevcutHizmet = _hizmetService.GetById(id);
+            if (mevcutHizmet == null)
+            {
+                return NotFound();
+            }
+
+            _hizmetService.HizmetSil(id);
+            return NoContent();
+        }
+    }
+}
